0085e6ad6a8f64484828d6778ca2b9da67622ed3
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-09-04 22:31:24 +0530

    Create Printing_in_python.py

---
+++ Printing_in_python.py
@@ -0,0 +1,19 @@
+#  Printing in python language
+# 1. anything in double or single quotes is string
+# 2. when we want to use english " " in between string we have 2 altenatives
+# - we can use single quote instead of double in in the biggining and end and double quote as english quote in between
+# - we can use "xyz" as english quote
+
+
+print("Hello world!")
+print("My name is PRAJNA")
+print('she said " hello"and then left')
+print("she said \"Hello\" and then left")
+print("she said 'hello' and then left ")
+print('she said \'hello\' and then left')
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print("hello world\nhello world\nhello wrold\n") # to print in next line
