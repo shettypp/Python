fec48ba8dc34cff63fbea8d4843d30e31a46a556
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-09-06 18:42:45 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,28 @@
+# VARIABLES  in python
+
+#when it comes to naming of variable
+# it should be readable
+# if there are 2 words either they should be written as single word or should be seaprated by underscore; there must not be any space in between any word
+# there can be number after variable i.g "length3" not before variale i.g "3length"
+
+name=input("what is your name?")
+print(name)
+
+name="ABC"
+print(name)
+
+name="XYZ"
+print(name)
+
+name=input("what is your name?")
+length=len(name)
+print(length)
+
+username="pqr"
+#or
+user_name="pqr"
+#but user name="pqr" is wrong
+length2="5"
+length2=5
+print(length2)
+#but 3lenght="5" is wrong
