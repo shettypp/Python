b200fad8421af78178a531163a6af57c25f48db1
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-09-08 22:49:31 +0530

    Create Brand name generator.py

---
+++ Brand name generator.py
@@ -0,0 +1,6 @@
+#Let us create a band name for a group of people who created a musical band by asking them few question
+
+print("Welcome to the Band Name Generator")
+city_name =input("what's the name of the city you grew up in?\n")
+pet_name =input("what's your pet name?\n")
+print("your Band Name could be "+city_name+" "+pet_name)
